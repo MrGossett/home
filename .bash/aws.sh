4afad6465b7ec9f678c4c62f21b7613c0f5316db
Tim Gossett <tim@mrgossett.com>
2016-09-21 19:23:10 -0400

    introduce aws-env and aws-unset

--- .bash/aws.sh
+++ .bash/aws.sh
@@ -12,3 +12,34 @@ function aws-profile() {
   fi
 }
 
+function aws-env() {
+  export AWS_DEFAULT_REGION="$(aws configure get region)"
+  export AWS_REGION="${AWS_DEFAULT_PROFILE}"
+
+  # optimistically fetch values using `aws configure` cli
+  id=$(aws configure get aws_access_key_id)
+  secret=$(aws configure get aws_secret_access_key)
+  token=""
+
+  # if those values are empty, then try to fetch from cli cache
+  if [ -z $id ]; then
+    # make a call so that CLI will possibly prompt for MFA and do sts:AssumeRole
+    aws sts get-caller-identity >/dev/null
+
+    json=$(
+      find $HOME/.aws/cli/cache/ -name ${AWS_DEFAULT_PROFILE}* | tail -n 1 \
+        | xargs cat
+    )
+    id=$(echo $json | jq -r '.Credentials.AccessKeyId')
+    secret=$(echo $json | jq -r '.Credentials.SecretAccessKey')
+    token=$(echo $json | jq -r '.Credentials.SessionToken')
+  fi
+
+  [ $id ] && export AWS_ACCESS_KEY_ID="$id"
+  [ $secret ] && export AWS_SECRET_ACCESS_KEY="$secret"
+  [ $token ] && export AWS_SESSION_TOKEN="$token" || unset AWS_SESSION_TOKEN
+}
+
+function aws-unset() {
+  unset $(env | grep AWS | grep -v PROFILE | cut -f 1 -d '=' | xargs)
+}
