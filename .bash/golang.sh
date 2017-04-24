c07196ef9678ae7421a84cf05cf97a19e9862f65
Tim Gossett <tgossett@magicleap.com>
2017-04-24 11:39:30 -0400

    drop install-go helper; put bitbucket before github in CDPATH

--- .bash/golang.sh
+++ .bash/golang.sh
@@ -1,26 +1,8 @@
-function install-go() {
-  local version=${1:-1.7}
-  mkdir -p /usr/local/src/go
-
-  [ -d "/usr/local/src/go/$version" ] && \
-    echo "Go $version is already installed" >&2 && \
-    return 1
-
-  [ ! -d /usr/local/src/go/1.4 ] && install-go "1.4"
-
-  git clone --branch "release-branch.go$version" --depth 1 \
-    git@github.com:golang/go.git "/usr/local/src/go/$version"
-  (
-    cd "/usr/local/src/go/${version}/src"
-    [[ "$version" > "1.4" ]] && export GOROOT_BOOTSTRAP=/usr/local/src/go/1.4
-    ./make.bash
-  ) 
-}
-
 # find all subdirectories of /usr/local/src/go, reverse-sort, and join with :
 export GOPATH=$(
   find /usr/local/src/go -type d -depth 1 | sort -r | xargs printf "%s:"
 )
+
 # set GOROOT to the first dir in GOPATH (should be the latest version of go)
 export GOROOT="${GOPATH%%:*}"
 
@@ -35,11 +17,12 @@ export GOBIN="${GOPATH%%:*}/bin"
 bins="${GOPATH//://bin:}/bin"
 [ "$PATH" != *"$bins"* ] && export PATH="$bins:$PATH"
 
-# append GOPATH to CDPATH, with /src/github.com
-# this allows `cd MrGossett` to act like `cd ~/go/src/github.com/MrGossett`
-export CDPATH="${CDPATH+$CDPATH:}${GOPATH//://src/github.com:}/src/github.com"
-# same for bitbucket.org
-export CDPATH="$CDPATH:${GOPATH//://src/bitbucket.org:}/src/bitbucket.org"
+# append GOPATH to CDPATH, with /src/bitbucket.org
+# this allows `cd MrGossett` to act like `cd ~/go/src/bitbucket.org/MrGossett`
+export CDPATH="${CDPATH+$CDPATH:}${GOPATH//://src/bitbucket.org:}/src/bitbucket.org"
+
+# same for github.com
+export CDPATH="$CDPATH:${GOPATH//://src/github.com:}/src/github.com"
 
 # make sure godoc is always running
 (godoc -http=:6060 2>/dev/null &)
