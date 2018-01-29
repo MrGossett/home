d92a3188e677c6d98b25e20f8bb578e8a0357239
Tim Gossett <tim@mrgossett.com>
2018-01-29 10:54:09 -0500

    no longer start gpg-agent since gpg itself will start it when needed

--- .bash/gpg.sh
+++ .bash/gpg.sh
@@ -1,9 +1,1 @@
-gpg-agent 2>/dev/null || \
-  eval "$(gpg-agent --daemon "${HOME}/.gpg-agent-info")"
-
-if [ -f "${HOME}/.gpg-agent-info" ]; then
-  source "${HOME}/.gpg-agent-info"
-  export GPG_AGENT_INFO
-fi
-
 export GPG_TTY="$(tty)"
