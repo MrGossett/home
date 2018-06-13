2f3af84b4dc63becd49f8a695ccd89672ca85475
Tim Gossett <tim@mrgossett.com>
2018-06-13 11:02:11 -0400

    use gpg-agent as drop-in for ssh-agent

--- .bash/gpg.sh
+++ .bash/gpg.sh
@@ -1,1 +1,8 @@
 export GPG_TTY="$(tty)"
+
+# use gpg-agent for SSH
+unset SSH_AGENT_PID
+if [ "${gnupg_SSH_AUTH_SOCK_by:-0}" -ne $$ ]; then
+  export SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
+fi
+
