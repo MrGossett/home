f5df9b4c17f024c0f1cad11706e88869f4aa7a38
Tim Gossett <tim@mrgossett.com>
2018-07-25 11:32:14 -0400

    tweak dockerpex

--- .bash/swapex.sh
+++ .bash/swapex.sh
@@ -1,6 +1,6 @@
-export SWAPEX_PARALLELISM=10
+export SWAPEX_PARALLELISM=100
 
-alias dockerpex="docker run --rm -it -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY -e AWS_SESSION_TOKEN -e AWS_DEFAULT_REGION -e AWS_REGION=\"\$AWS_DEFAULT_REGION\" -e SWAPEX_ENVIRONMENT --workdir /app -v \$(pwd):/app --entrypoint /bin/bash swapex -l"
+alias dockerpex="docker run --rm -it -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY -e AWS_SESSION_TOKEN -e AWS_DEFAULT_REGION -e AWS_REGION=\"\$AWS_DEFAULT_REGION\" -e SWAPEX_ENVIRONMENT --workdir /app -v \$(pwd):/app --entrypoint /bin/bash swapex"
 
 function s3vim() {
   s3url=$1
