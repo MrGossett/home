6553e0fcfd93dd1454905423aeed5cf2fff5fc16
Tim Gossett <tim@mrgossett.com>
2018-08-08 00:16:48 -0400

    drop some PATH stuff

--- .bash/golang.sh
+++ .bash/golang.sh
@@ -1,9 +1,3 @@
-export GOROOT="$(find $HOME -maxdepth 1 -type d -name 'go1.*' | sort -Vr | head -n1)"
-[ "$PATH" != *"$GOROOT/bin"* ] && export PATH="$GOROOT/bin:$PATH"
-
-export GOPATH="$HOME/go"
-[ "$PATH" != *"$GOPATH/bin"* ] && export PATH="$GOPATH/bin:$PATH"
-
 # append GOPATH to CDPATH, with /src/bitbucket.org
 # this allows `cd MrGossett` to act like `cd ~/go/src/bitbucket.org/MrGossett`
 export CDPATH="${CDPATH+$CDPATH:}${GOPATH//://src/bitbucket.org:}/src/bitbucket.org"
