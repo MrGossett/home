076829b2e9cb9ed567a7ba60917efaa81a6f3814
Tim Gossett <tim@mrgossett.com>
2018-08-17 18:12:48 -0400

    bring back GOPATH so that GOBIN is in PATH

--- .bash/golang.sh
+++ .bash/golang.sh
@@ -1,6 +1,10 @@
+export GOPATH="$HOME/go"
+
 # append GOPATH to CDPATH, with /src/bitbucket.org
 # this allows `cd MrGossett` to act like `cd ~/go/src/bitbucket.org/MrGossett`
 export CDPATH="${CDPATH+$CDPATH:}${GOPATH//://src/bitbucket.org:}/src/bitbucket.org"
 
 # same for github.com
 export CDPATH="$CDPATH:${GOPATH//://src/github.com:}/src/github.com"
+
+export PATH="$PATH:$GOPATH/bin"
