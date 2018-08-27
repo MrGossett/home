212773e47791990767a3d72ee0d749bf41594f84
Tim Gossett <tim@mrgossett.com>
2018-08-27 14:57:41 -0400

    introduce launchd agents for offlineimap

--- .bash/tmux.sh
+++ .bash/tmux.sh
@@ -1,8 +1,5 @@
 if [[ `uname` == 'Darwin' ]]; then
   # setup local pasteboard service
-  if [[ ! -f "$HOME/Library/LaunchAgents/pbcopy.plist" ]]; then
-    ln -s "$HOME/.tmux/pbcopy.plist" "$HOME/Library/LaunchAgents/pbcopy.plist"
-  fi
   launchctl load "$HOME/Library/LaunchAgents/pbcopy.plist" 2>/dev/null && \
     launchctl start local.pbcopy
 fi
