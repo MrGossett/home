c1a64fb93ca470e7261e3354b468e8ca1abdc8df
Tim Gossett <tim@mrgossett.com>
2018-09-05 16:49:05 -0400

    fix bash tab completion

--- .bash/brewpath.sh
+++ .bash/brewpath.sh
@@ -2,3 +2,5 @@
   export PATH="$PATH:/usr/local/bin"
 [ -d /usr/local/sbin ] && [[ "$PATH" != *"/usr/local/sbin"* ]] && \
   export PATH="$PATH:/usr/local/sbin"
+[ -f /usr/local/etc/bash_completion ] && \
+  . /usr/local/etc/bash_completion
