315ba9d9889c513ef9c266c22cc7a8324c66dbf9
Tim Gossett <tim@mrgossett.com>
2020-11-02 10:28:27 -0500

    add go-{un,}covered-exported functions

--- .bash/golang.sh
+++ .bash/golang.sh
@@ -20,4 +20,18 @@ function godoc() {
     bash -c "go get golang.org/x/tools/cmd/godoc && echo http://localhost:6060/pkg/$module && /tmp/go/bin/godoc -http=:6060"
 }
 
+function go-uncovered-exported() {
+  out=$(mktemp)
+  go test -coverprofile=$out .
+  go tool cover -func=$out | grep -v -e 'total:' -e '100.0%' | sort -hrk 3 -k 1 |  awk '$2 ~ /^[A-Z]/'
+}
+
+function go-covered-exported() {
+  out=$(mktemp)
+  go test -coverprofile=$out .
+  go tool cover -func=$out | grep -v -e 'total:' | grep -e '100.0%' | sort -hrk 3 -k 1 |  awk '$2 ~ /^[A-Z]/'
+}
+
 source <(golangci-lint completion bash)
+
+complete -C "$HOME/go/bin/gocomplete" go
